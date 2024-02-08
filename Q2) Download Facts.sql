1d35fb47ba4167d81039f4e6dc20c7604898112d
Roopa Gupta <roopa164gupta@gmail.com>
2024-02-08 11:11:00 -0800

    Update and rename Download Facts.sql to Q2) Download Facts.sql

--- Q2) Download Facts.sql
+++ Q2) Download Facts.sql
@@ -1,4 +1,4 @@
-// Question :https://www.interviewquery.com/questions/download-facts?solution=true&utm_medium=email&utm_source=weekly_question
+
 select d.download_date , a.paying_customer , round(avg(d.downloads),2) average_downloads
 from accounts a left join downloads d
 on a.account_id = d.account_id
